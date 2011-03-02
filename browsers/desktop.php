cdb0baf41fafe63c3200695e448946d74285d66d
liruqi <liruqi@gmail.com>
2011-03-02 16:57:24 +0800

    use dabr browsers in head version

--- browsers/desktop.php
+++ browsers/desktop.php
@@ -1,17 +1,43 @@
 <?php
 function desktop_theme_status_form($text = '', $in_reply_to_id = NULL) {
-  if (user_is_authenticated()) {
-    $output = '<form method="post" action="update" onsubmit="return confirmShortTweet();">
-  <textarea id="status" name="status" rows="3" style="width:100%; max-width: 400px;">'.$text.'</textarea>
-  <div><input name="in_reply_to_id" value="'.$in_reply_to_id.'" type="hidden" /><input type="submit" value="Update" /> <span id="remaining">140</span></div>
+	if (user_is_authenticated()) {
+		$output = '<form method="post" action="update">
+  <textarea id="status" name="status" rows="3" style="width:95%; max-width: 400px;">'.$text.'</textarea>
+  <div><input name="in_reply_to_id" value="'.$in_reply_to_id.'" type="hidden" /><input type="submit" value="Update" /> <span id="remaining">140</span> 
+  <span id="geo" style="display: none; float: right;"><input onclick="goGeo()" type="checkbox" id="geoloc" name="location" /> <label for="geoloc" id="lblGeo"></label></span></div>
+  <script type="text/javascript">
+started = false;
+chkbox = document.getElementById("geoloc");
+if (navigator.geolocation) {
+	geoStatus("Tweet my location");
+	if ("'.$_COOKIE['geo'].'"=="Y") {
+		chkbox.checked = true;
+		goGeo();
+	}
+}
+function goGeo(node) {
+	if (started) return;
+	started = true;
+	geoStatus("Locating...");
+	navigator.geolocation.getCurrentPosition(geoSuccess, geoStatus);
+}
+function geoStatus(msg) {
+	document.getElementById("geo").style.display = "inline";
+	document.getElementById("lblGeo").innerHTML = msg;
+}
+function geoSuccess(position) {
+	geoStatus("Tweet my <a href=\'http://maps.google.co.uk/m?q=" + position.coords.latitude + "," + position.coords.longitude + "\' target=\'blank\'>location</a>");
+	chkbox.value = position.coords.latitude + "," + position.coords.longitude;
+}
+  </script>
 </form>';
-    $output .= js_counter('status');
-    return $output;
-  }
+		$output .= js_counter('status');
+		return $output;
+	}
 }
 
 function desktop_theme_search_form($query) {
-  $query = stripslashes(htmlentities($query,ENT_QUOTES,"UTF-8"));
-  return "<form action='search' method='get'><input name='query' value=\"$query\" style='width:100%; max-width: 300px' /><input type='submit' value='Search' /></form>";
+	$query = stripslashes(htmlentities($query,ENT_QUOTES,"UTF-8"));
+	return "<form action='search' method='get'><input name='query' value=\"$query\" style='width:100%; max-width: 300px' /><input type='submit' value='Search' /></form>";
 }
 ?>
--- browsers/touch.php
+++ browsers/touch.php
@@ -2,70 +2,91 @@
 
 require 'desktop.php';
 
+function touch_theme_action_icon($url, $image_url, $text) {
+	$image_url = str_replace('.png', 'L.png', $image_url);
+	$image_url = str_replace('.gif', 'L.png', $image_url);
+	if ($text == 'MAP')	{
+		return "<a href='$url' target='_blank'><img src='$image_url' /></a>";
+	}
+	return "<a href='$url'><img src='$image_url' width='12' height='12' /></a>";
+}
+
+
 function touch_theme_status_form($text = '', $in_reply_to_id = NULL) {
-  return desktop_theme_status_form($text, $in_reply_to_id);
+	return desktop_theme_status_form($text, $in_reply_to_id);
 }
 function touch_theme_search_form($query) {
-  return desktop_theme_search_form($query);
+	return desktop_theme_search_form($query);
 }
 
 function touch_theme_avatar($url, $force_large = false) {
-  return "<img src='$url' width='48' height='48' />";
+	return "<img src='$url' width='48' height='48' />";
 }
 
 function touch_theme_page($title, $content) {
-  $body = theme('menu_top');
-  $body .= $content;
-  $body .= theme('google_analytics');
-  ob_start('ob_gzhandler');
-  header('Content-Type: text/html; charset=utf-8');
-  echo '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head><meta name="viewport" content="width=320"/><title>',$title,'</title><base href="',BASE_URL,'" />
-'.theme('css').'
-</head>
-<body id="thepage">', $body, '</body>
-</html>';
-  exit();
+	$body = theme('menu_top');
+	$body .= $content;
+	$body .= theme('google_analytics');
+	ob_start('ob_gzhandler');
+	header('Content-Type: text/html; charset=utf-8');
+	echo 	'<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+		<html xmlns="http://www.w3.org/1999/xhtml">
+			<head>
+				<meta name="viewport" content="width=device-width; initial-scale=1.0;" />
+				<title>',$title,'</title>
+				<base href="',BASE_URL,'" />
+				'.theme('css').'
+			</head>
+			<body id="thepage">';
+
+        if (file_exists("common/admob.php"))
+        {
+		                echo '<div class="advert">';
+                		require_once("common/admob.php");
+		                echo '</div>';
+        }
+        echo			$body,
+			'</body>
+		</html>';
+        exit();
 }
 
 function touch_theme_menu_top() {
-  $links = array();
-  $main_menu_titles = array('home', 'mentions', 'cmts', 'directs');
-  foreach (menu_visible_items() as $url => $page) {
-    $title = $url ? $url : 'home';
-    $type = in_array($title, $main_menu_titles) ? 'main' : 'extras';
-    $links[$type][] = "<a href='$url'>$title</a>";
-  }
-  if (user_is_authenticated()) {
-    $user = user_current_username();
-    array_unshift($links['extras'], "<b><a href='user/$user'>$user</a></b>");
-  }
-  array_push($links['main'], '<a href="#" onclick="return toggleMenu()">more</a>');
-  $html = '<div id="menu" class="menu">';
-  $html .= theme('list', $links['main'], array('id' => 'menu-main'));
-  $html .= theme('list', $links['extras'], array('id' => 'menu-extras'));
-  $html .= '</div>';
-  return $html;
+	$links = array();
+	$main_menu_titles = array('home', 'replies', 'directs', 'search');
+	foreach (menu_visible_items() as $url => $page) {
+		$title = $url ? $url : 'home';
+		$type = in_array($title, $main_menu_titles) ? 'main' : 'extras';
+		$links[$type][] = "<a href='$url'>$title</a>";
+	}
+	if (user_is_authenticated()) {
+		$user = user_current_username();
+		array_unshift($links['extras'], "<b><a href='user/$user'>$user</a></b>");
+	}
+	array_push($links['main'], '<a href="#" onclick="return toggleMenu()">more</a>');
+	$html = '<div id="menu" class="menu">';
+	$html .= theme('list', $links['main'], array('id' => 'menu-main'));
+	$html .= theme('list', $links['extras'], array('id' => 'menu-extras'));
+	$html .= '</div>';
+	return $html;
 }
 
 function touch_theme_menu_bottom() {
-  return '';
+	return '';
 }
 
 function touch_theme_status_time_link($status, $is_link = true) {
-  $out = theme_status_time_link($status, $is_link);
-  $out = str_replace(array(' ago', ' years', ' days', ' hours', ' min', ' sec'),
-                     array('', 'y', 'd', 'h', 'm', 's'), $out);
-  return $out;
+	$out = theme_status_time_link($status, $is_link);
+	//old method didn't work with conversation view (and no longer with correct pluralisation)
+	$out = str_replace(array(' years ago', ' year ago', ' days ago', ' day ago', ' hours ago', ' hour ago', ' mins ago', ' min ago', ' secs ago', ' sec ago'),
+	                   array('y', 'y', 'd', 'd', 'h', 'h', 'm', 'm', 's', 's'), $out);
+	return $out;
 }
 
-
 function touch_theme_css() {
-  $out = '<link rel="stylesheet" href="browsers/touch.css" />';
-  //~ $out .= '<style type="text/css">body { word-wrap: break-word; text-overflow: ellipsis; } table {width: 320px;}</style>';
-  $out .= theme_css();
-  $out .= '<script type="text/javascript">'.file_get_contents('browsers/touch.js').'</script>';
-  return $out;
+	$out = theme_css();
+	$out .= '<link rel="stylesheet" href="browsers/touch.css" />';
+	$out .= '<script type="text/javascript">'.file_get_contents('browsers/touch.js').'</script>';
+	return $out;
 }
 ?>
