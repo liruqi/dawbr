429df2fd6d7c76281af3848608c5d14caefdb05e
liruqi <liruqi@gmail.com>
2011-03-20 15:44:13 +0800

    update config

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 define('API_URL','http://api.t.sina.com.cn/');
 
 // Cookie encryption key. Max 52 characters
-define('ENCRYPTION_KEY', 'Example Key Change Me!');
+define('ENCRYPTION_KEY', 'Example Key - Change Me!');
 
 // OAuth consumer and secret keys. Available from http://twitter.com/oauth_clients
 define('OAUTH_CONSUMER_KEY', '4186909475');
@@ -21,6 +21,9 @@ define('FLICKR_API_KEY', '');
 // Uncomment to enable.
 // define('LONGURL_KEY', 'true');
 
+// Optional: Enable to view page processing and API time
+define('DEBUG_MODE', 'OFF');
+
 // Base URL, should point to your website, including a trailing slash
 // Can be set manually but the following code tries to work it out automatically.
 $base_url = 'http://'.$_SERVER['HTTP_HOST'];
@@ -30,6 +33,24 @@ if ($directory = trim(dirname($_SERVER['SCRIPT_NAME']), '/\,')) {
 define('BASE_URL', $base_url.'/');
 
 
+
+// MySQL storage of OAuth login details for users
+define('MYSQL_USERS', 'OFF');
+// mysql_connect('localhost', 'username', 'password');
+// mysql_select_db('dabr');
+
+/* The following table is needed to store user login details if you enable MYSQL_USERS:
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(64) NOT NULL,
+  `oauth_key` varchar(128) NOT NULL,
+  `oauth_secret` varchar(128) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`username`)
+)
+
+*/
+
 // Google Analytics Mobile tracking code
 // You need to download ga.php from the Google Analytics website for this to work
 // Copyright 2009 Google Inc. All Rights Reserved.
@@ -37,23 +58,23 @@ $GA_ACCOUNT = "";
 $GA_PIXEL = "ga.php";
 
 function googleAnalyticsGetImageUrl() {
-  global $GA_ACCOUNT, $GA_PIXEL;
-  $url = "";
-  $url .= $GA_PIXEL . "?";
-  $url .= "utmac=" . $GA_ACCOUNT;
-  $url .= "&utmn=" . rand(0, 0x7fffffff);
-  $referer = $_SERVER["HTTP_REFERER"];
-  $query = $_SERVER["QUERY_STRING"];
-  $path = $_SERVER["REQUEST_URI"];
-  if (empty($referer)) {
-    $referer = "-";
-  }
-  $url .= "&utmr=" . urlencode($referer);
-  if (!empty($path)) {
-    $url .= "&utmp=" . urlencode($path);
-  }
-  $url .= "&guid=ON";
-  return str_replace("&", "&amp;", $url);
+	global $GA_ACCOUNT, $GA_PIXEL;
+	$url = "";
+	$url .= $GA_PIXEL . "?";
+	$url .= "utmac=" . $GA_ACCOUNT;
+	$url .= "&utmn=" . rand(0, 0x7fffffff);
+	$referer = $_SERVER["HTTP_REFERER"];
+	$query = $_SERVER["QUERY_STRING"];
+	$path = $_SERVER["REQUEST_URI"];
+	if (empty($referer)) {
+		$referer = "-";
+	}
+	$url .= "&utmr=" . urlencode($referer);
+	if (!empty($path)) {
+		$url .= "&utmp=" . urlencode($path);
+	}
+	$url .= "&guid=ON";
+	return str_replace("&", "&amp;", $url);
 }
 
 ?>
