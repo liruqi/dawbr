c830d7f53ec8a3ebfd151018a26846e91a29aa9a
liruqi <liruqi@gmail.com>
2011-03-20 15:52:19 +0800

    merge theme from dabr

--- common/theme.php
+++ common/theme.php
@@ -3,63 +3,63 @@
 $current_theme = false;
 
 function theme() {
-  global $current_theme;
-  $args = func_get_args();
-  $function = array_shift($args);
-  $function = 'theme_'.$function;
-  
-  if ($current_theme) {
-    $custom_function = $current_theme.'_'.$function;
-    if (function_exists($custom_function))
-      $function = $custom_function;
-  } else {
-    if (!function_exists($function))
-      return "<p>Error: theme function <b>$function</b> not found.</p>";
-  }
-  return call_user_func_array($function, $args);
+	global $current_theme;
+	$args = func_get_args();
+	$function = array_shift($args);
+	$function = 'theme_'.$function;
+
+	if ($current_theme) {
+		$custom_function = $current_theme.'_'.$function;
+		if (function_exists($custom_function))
+		$function = $custom_function;
+	} else {
+		if (!function_exists($function))
+		return "<p>Error: theme function <b>$function</b> not found.</p>";
+	}
+	return call_user_func_array($function, $args);
 }
 
 function theme_csv($headers, $rows) {
-  $out = implode(',', $headers)."\n";
-  foreach ($rows as $row) {
-    $out .= implode(',', $row)."\n";
-  }
-  return $out;
+	$out = implode(',', $headers)."\n";
+	foreach ($rows as $row) {
+		$out .= implode(',', $row)."\n";
+	}
+	return $out;
 }
 
 function theme_list($items, $attributes) {
-  if (!is_array($items) || count($items) == 0) {
-    return '';
-  }
-  $output = '<ul'.theme_attributes($attributes).'>';
-  foreach ($items as $item) {
-    $output .= "<li>$item</li>\n";
-  }
-  $output .= "</ul>\n";
-  return $output;
+	if (!is_array($items) || count($items) == 0) {
+		return '';
+	}
+	$output = '<ul'.theme_attributes($attributes).'>';
+	foreach ($items as $item) {
+		$output .= "<li>$item</li>\n";
+	}
+	$output .= "</ul>\n";
+	return $output;
 }
 
 function theme_options($options, $selected = NULL) {
-  if (count($options) == 0) return '';
-  $output = '';
-  foreach($options as $value => $name) {
-    if (is_array($name)) {
-      $output .= '<optgroup label="'.$value.'">';
-      $output .= theme('options', $name, $selected);
-      $output .= '</optgroup>';
-    } else {
-      $output .= '<option value="'.$value.'"'.($selected == $value ? ' selected="selected"' : '').'>'.$name."</option>\n";
-    }
-  }
-  return $output;
+	if (count($options) == 0) return '';
+	$output = '';
+	foreach($options as $value => $name) {
+		if (is_array($name)) {
+			$output .= '<optgroup label="'.$value.'">';
+			$output .= theme('options', $name, $selected);
+			$output .= '</optgroup>';
+		} else {
+			$output .= '<option value="'.$value.'"'.($selected == $value ? ' selected="selected"' : '').'>'.$name."</option>\n";
+		}
+	}
+	return $output;
 }
 
 function theme_info($info) {
-  $rows = array();
-  foreach ($info as $name => $value) {
-    $rows[] = array($name, $value);
-  }
-  return theme('table', array(), $rows);
+	$rows = array();
+	foreach ($info as $name => $value) {
+		$rows[] = array($name, $value);
+	}
+	return theme('table', array(), $rows);
 }
 
 function theme_table($headers, $rows, $attributes = NULL) {
@@ -102,11 +102,11 @@ function theme_table_rows($rows) {
 }
 
 function theme_attributes($attributes) {
-  if (!$attributes) return;
-  foreach ($attributes as $name => $value) {
-    $out .= " $name=\"$value\"";
-  }
-  return $out;
+	if (!$attributes) return;
+	foreach ($attributes as $name => $value) {
+		$out .= " $name=\"$value\"";
+	}
+	return $out;
 }
 
 function theme_table_cell($contents, $header = FALSE) {
@@ -124,7 +124,7 @@ function theme_table_cell($contents, $header = FALSE) {
 
 
 function theme_error($message) {
-  theme_page('Error', $message);
+	theme_page('Error', $message);
 }
 
 function theme_page($title, $content) {
@@ -166,43 +166,53 @@ function theme_page($title, $content) {
 }
 
 function theme_colours() {
-  $info = $GLOBALS['colour_schemes'][setting_fetch('colours', 5)];
-  list($name, $bits) = explode('|', $info);
-  $colours = explode(',', $bits);
-  return (object) array(
-    'links' => $colours[0],
-    'bodybg' => $colours[1],
-    'bodyt' => $colours[2],
-    'small' => $colours[3],
-    'odd' => $colours[4],
-    'even' => $colours[5],
-    'replyodd' => $colours[6],
-    'replyeven' => $colours[7],
-    'menubg' => $colours[8],
-    'menut' => $colours[9],
-    'menua' => $colours[10],
-  );
+	$info = $GLOBALS['colour_schemes'][setting_fetch('colours', 5)];
+	list($name, $bits) = explode('|', $info);
+	$colours = explode(',', $bits);
+	return (object) array(
+		'links' => $colours[0],
+		'bodybg' => $colours[1],
+		'bodyt' => $colours[2],
+		'small' => $colours[3],
+		'odd' => $colours[4],
+		'even' => $colours[5],
+		'replyodd' => $colours[6],
+		'replyeven' => $colours[7],
+		'menubg' => $colours[8],
+		'menut' => $colours[9],
+		'menua' => $colours[10],
+	);
 }
 
 function theme_css() {
-  $c = theme('colours');
-  return "<style type='text/css'>
+	$c = theme('colours');
+	return "<style type='text/css'>
   a{color:#{$c->links}}table{border-collapse:collapse}
   form{margin:.3em;}td{vertical-align:top;padding:0.3em}img{border:0}small,small a{color:#{$c->small}}
   body{background:#{$c->bodybg};
   color:#{$c->bodyt};margin:0;font:90% sans-serif}tr.odd td{background:#{$c->odd}}tr.even td{background:#{$c->even}}
-  tr.reply td{background:#{$c->replyodd}
-  }tr.reply.even td{background: #{$c->replyeven}}
+  .odd{background:#{$c->odd}}
+  .even{background:#{$c->even}}
+  .reply{background:#{$c->replyodd}}
+  .reply.even{background: #{$c->replyeven}}
   .menu{color:#{$c->menut};background:#{$c->menubg};padding: 2px}
   .menu a{color:#{$c->menua};text-decoration: none}
+  .tweet,.features{padding:5px}
+  .date{padding:5px;font-size:0.8em;font-weight:bold;color:#{$c->small}}
+  .about,.time{font-size:0.75em;color:#{$c->small}}
+  .avatar{display:block; height:26px; width:26px; left:0.3em; margin:0; overflow:hidden; position:absolute;}
+  .status{display:block;}
+  .shift{margin-left:30px;min-height:24px;}
+  .from{font-size:0.75em;color:#{$c->small};font-family:serif;}
+  .from a{color:#{$c->small};}
 </style>";
 }
 
 function theme_google_analytics() {
-  global $GA_ACCOUNT;
-  if (!$GA_ACCOUNT) return '';
-  $googleAnalyticsImageUrl = googleAnalyticsGetImageUrl();
-  return "<img src='{$googleAnalyticsImageUrl}' />";
+	global $GA_ACCOUNT;
+	if (!$GA_ACCOUNT) return '';
+	$googleAnalyticsImageUrl = googleAnalyticsGetImageUrl();
+	return "<img src='{$googleAnalyticsImageUrl}' />";
 }
 
 ?>
